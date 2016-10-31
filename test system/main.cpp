1e8e7d8a8323c79bcd5010281fd26fb406cebfb6
陈晖 <chenhui@chenhuideMacBook-Pro.local>
2016-10-31 19:15:55 +0800

    Initial Commit

---
+++ test system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test system
+//
+//  Created by 陈晖 on 2016/10/31.
+//  Copyright © 2016年 Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
